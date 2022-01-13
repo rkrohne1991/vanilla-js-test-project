2cef6d16c450dd2964533e847527ff1e1c3c52be
Rafal Krohne <rafal.krohne@hotmail.co.uk>
2022-01-13 12:59:40 +0100

    Adding rules for style, css and images + testing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import _ from "lodash";
+import printMe from "./print.js";
+
+import "./style.css";
+import Icon from "./icon.png";
 
 if (process.env.NODE_ENV !== "production") {
   console.log("Looks like we are in development mode!");
@@ -6,9 +10,22 @@ if (process.env.NODE_ENV !== "production") {
 
 function component() {
   const element = document.createElement("div");
+  const btn = document.createElement("button");
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
